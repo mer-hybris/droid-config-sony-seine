081677a6fb727287238e2b70e8e60ae663955889
Frajo Haider <f_haider@gmx.at>
2019-11-12 17:30:14 +0200

    radio: remove the now obsolte modem workaround.
    radio: remove the now obsolete modem workaround. JB#48074

--- sparse/usr/bin/droid/fixup_ril.sh
+++ sparse/usr/bin/droid/fixup_ril.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 
-mkdir -p /dev/socket/qmux_radio/rild_sync_1
-
-# single sim workaround
-(
-
+# Restore single sim devices after removal of the modem workaround.
 if [ "$(grep -o persist.vendor.radio.multisim.config=dsds /vendor/build.prop)" != "persist.vendor.radio.multisim.config=dsds" ]; then
     while [ "$(pidof rild)" == "" ]; do
         echo "Waiting for rild"
         sleep 1
     done
 
+    /system/bin/stop vendor.ril-daemon2
+    setprop persist.vendor.radio.multisim.config ""
     setprop persist.radio.multisim.config ""
 fi
 
-) &
